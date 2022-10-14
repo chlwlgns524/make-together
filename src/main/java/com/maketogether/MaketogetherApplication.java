6c9e235f6f22efe25939bf19e5aa2af9dc4609a1
chlwlgns524 <chlwlgns524@naver.com>
2022-10-14 09:47:57 +0900

    [Chore] 프로젝트 초기화

---
+++ src/main/java/com/maketogether/MaketogetherApplication.java
@@ -0,0 +1,13 @@
+package com.maketogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaketogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaketogetherApplication.class, args);
+    }
+
+}
