3f3067ea354c1be647952e4163b06a78230dd688
chlwlgns524 <chlwlgns524@naver.com>
2022-10-21 16:26:04 +0900

    [feature] 시간에 관련한 공통 속성을 지닌 BaseTimeEntity 정의

---
+++ src/main/java/com/maketogether/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.maketogether.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime expiredAt;
+
+}
